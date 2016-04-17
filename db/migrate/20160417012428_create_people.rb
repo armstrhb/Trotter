42aa6e76fca8603f6b2fe2291ff503a18d62dce4
armstrhb <henry.b.armstrong@gmail.com>
2016-04-16 21:59:36 -0400

    people migrate

--- db/migrate/20160417012428_create_people.rb
+++ db/migrate/20160417012428_create_people.rb
@@ -1,7 +1,12 @@
 class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :nick_name
+      t.date :birthday
+      t.string :relationship_description
+      t.text :notes
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+henry = Person.create({first_name: 'Henry', last_name: 'Armstrong', nick_name: 'Bo', birthday: DateTime.strptime('04/02/2010', '%m/%d/%Y')})
+mattie = Person.create({first_name: 'Matthew', last_name: 'Armstrong', nick_name: 'Mattie', birthday: DateTime.strptime('12/24/2011', '%m/%d/%Y')})
+mom = Person.create({first_name: 'Jan', last_name: 'Armstrong', nick_name: 'Mom', birthday: DateTime.strptime('11/01/1946', '%m/%d/%Y')})
+dad = Person.create({first_name: 'Henry', last_name: 'Armstrong', nick_name: 'Dad', birthday: DateTime.strptime('11/27/1951', '%m/%d/%Y')})
